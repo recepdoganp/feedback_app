da2e03357be97a05a01e54ddbc44fa0e7bed84aa
recepdoganp <rec.do.pezukoglu@hotmail.com>
2022-09-30 00:34:22 +0300

    Form Created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
+import FeedbackStats from "./components/FeedbackStats";
+import FeedbackForm from "./components/FeedbackForm";
 import FeedbackData from "./data/FeedbackData";
 
 function App() {
@@ -15,6 +17,8 @@ function App() {
     <>
       <Header />
       <div className='container'>
+        <FeedbackForm />
+        <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
