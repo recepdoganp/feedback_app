4a70d7821b5190a10125593785d35987dc3da753
recepdoganp <rec.do.pezukoglu@hotmail.com>
2022-10-12 01:20:28 +0300

    Update feedback

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -7,10 +7,22 @@ export const FeedbackProvider = ({ children }) => {
   const [feedback, setFeedback] = useState([
     {
       id: 1,
-      text: "This item is from context",
+      text: "This feedback item 1",
       rating: 9,
     },
+    {
+      id: 2,
+      text: "This feedback item 2",
+      rating: 8,
+    },
+    {
+      id: 3,
+      text: "This feedback item 3",
+      rating: 6,
+    },
   ]);
+
+  const [feedbackEdit, setFeedbackEdit] = useState({ item: {}, edit: false });
 
   const deleteFeedback = (id) => {
     if (window.confirm("Are you sure you want to delete")) {
@@ -23,8 +35,27 @@ export const FeedbackProvider = ({ children }) => {
     setFeedback([newFeedback, ...feedback]);
   };
 
+  const editFeedback = (item) => {
+    setFeedbackEdit({ item, edit: true });
+  };
+
+  const updateFeedback = (id, updItem) => {
+    setFeedback(
+      feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item))
+    );
+  };
+
   return (
-    <FeedbackContext.Provider value={{ feedback, deleteFeedback, addFeedback }}>
+    <FeedbackContext.Provider
+      value={{
+        feedback,
+        feedbackEdit,
+        deleteFeedback,
+        addFeedback,
+        editFeedback,
+        updateFeedback,
+      }}
+    >
       {children}
     </FeedbackContext.Provider>
   );
